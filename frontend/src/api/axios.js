f0fe4b5de2cd4961a9bb610e74e296bc207e836c
J. Angel G. <angel.gonzalez.rosas@gmail.com>
2026-04-12 04:06:56 -0600

    fix: usar variable de entorno para URL de API en producción

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api', // Adjust the base URL as needed
+    baseURL: 'http://localhost:8000/api' || import.meta.env.VITE_API_URL, // Adjust the base URL as needed
     headers: {
         'Content-Type': 'application/json',
     },
